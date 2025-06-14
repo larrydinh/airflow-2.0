1325b2d890e79b1d56b5df440e3d21353e6140d7
Larry <huuphucdinh@gmail.com>
2025-06-14 15:17:31 +0200

    check api available using BaseHook get connection - set Connection in localhost

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -1,4 +1,6 @@
 from airflow.decorators import dag, task
+from airflow.hooks.base import BaseHook
+from airflow.sensors.base import PokeReturnValue
 from datetime import datetime
 
 @dag(
@@ -9,7 +11,16 @@ from datetime import datetime
     tags = ['stock_market']
 )
 def stock_market():
-    # @task.sensor()
-    pass
+    @task.sensor(poke_interval =30, timeout=300, mode = 'poke')
+    def is_api_available() -> PokeReturnValue:
+        import requests
+        api = BaseHook.get_connection('stock_api')
+        url = f"{api.host}{api.extra_dejson['endpoint']}"
+        print(url)
+        response = requests.get(url, headers=api.extra_dejson['headers'])
+        condition = response.json()['finance']['result'] is None
+        return PokeReturnValue(is_done = condition, xcom_value = url)
+    # pass
+    is_api_available()
 
 stock_market()
