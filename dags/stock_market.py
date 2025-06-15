f9c60b2f8fa0efec13f9b01577df180d9a045c95
Larry <huuphucdinh@gmail.com>
2025-06-15 14:59:26 +0200

    save stock price in minio, json format

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -4,7 +4,7 @@ from airflow.sensors.base import PokeReturnValue
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 
-from include.stock_market.tasks import _get_stock_prices
+from include.stock_market.tasks import _get_stock_prices, _store_prices
 SYMBOL = 'NVDA'
 
 @dag(
@@ -34,7 +34,12 @@ def stock_market():
     op_kwargs={'url': '{{ ti.xcom_pull(task_ids="is_api_available") }}', 'symbol': SYMBOL}
     #{{task}}: task between 2 curly bracket will only be evaluated when the task run
     )
-    
-    is_api_available() >> get_stock_prices
+
+    store_prices = PythonOperator(
+        task_id='store_prices',
+        python_callable=_store_prices,
+        op_kwargs={'stock': '{{ ti.xcom_pull(task_ids="get_stock_prices") }}'}
+    )
+    is_api_available() >> get_stock_prices >> store_prices
 
 stock_market()
