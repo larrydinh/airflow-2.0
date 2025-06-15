ac703a5f2204157c507454b4892500f101536735
Larry <huuphucdinh@gmail.com>
2025-06-15 11:25:45 +0200

    get stock price from yahoo api

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -1,7 +1,11 @@
 from airflow.decorators import dag, task
 from airflow.hooks.base import BaseHook
 from airflow.sensors.base import PokeReturnValue
+from airflow.operators.python import PythonOperator
 from datetime import datetime
+
+from include.stock_market.tasks import _get_stock_prices
+SYMBOL = 'NVDA'
 
 @dag(
     start_date=datetime(2023,1,1),
@@ -12,15 +16,25 @@ from datetime import datetime
 )
 def stock_market():
     @task.sensor(poke_interval =30, timeout=300, mode = 'poke')
+    #this def use to detect whether the API is available or not. If API url return null after called then API is available
+
     def is_api_available() -> PokeReturnValue:
         import requests
-        api = BaseHook.get_connection('stock_api')
-        url = f"{api.host}{api.extra_dejson['endpoint']}"
+        api = BaseHook.get_connection('stock_api') #  Gets Airflow connection details
+        url = f"{api.host}{api.extra_dejson['endpoint']}"  # Constructs full URL
         print(url)
-        response = requests.get(url, headers=api.extra_dejson['headers'])
+        response = requests.get(url, headers=api.extra_dejson['headers']) # Sends GET request
         condition = response.json()['finance']['result'] is None
         return PokeReturnValue(is_done = condition, xcom_value = url)
-    # pass
-    is_api_available()
+
+    #use traditional PythonOperator here since it will be tricky when decorator working with Docker operators as well.
+    get_stock_prices = PythonOperator(
+    task_id='get_stock_prices',
+    python_callable=_get_stock_prices,
+    op_kwargs={'url': '{{ ti.xcom_pull(task_ids="is_api_available") }}', 'symbol': SYMBOL}
+    #{{task}}: task between 2 curly bracket will only be evaluated when the task run
+    )
+    
+    is_api_available() >> get_stock_prices
 
 stock_market()
---
+++ include/stock_market/tasks.py
@@ -0,0 +1,12 @@
+from airflow.hooks.base import BaseHook
+
+def _get_stock_prices(url, symbol):
+    import requests
+    import json
+    #symbol: company
+    url = f"{url}{symbol}?metrics=high?&interval=1d&range=1y"
+    api = BaseHook.get_connection('stock_api')
+    response = requests.get(url, headers=api.extra_dejson['headers'])
+    return json.dumps(response.json()['chart']['result'][0])
+    
+# def _store_prices():
