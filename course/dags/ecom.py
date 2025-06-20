a9a6d5b3e162107b9ad4ad9800095ffb91e71ba6
Larry <huuphucdinh@gmail.com>
2025-06-20 22:37:35 +0200

    backfill and idempotency

--- course/dags/ecom.py
+++ course/dags/ecom.py
@@ -1,6 +1,7 @@
 from airflow.decorators import dag
-from airflow.operators.python import PythonOperator
-from pendulum import datetime, timedelta
+from airflow.operators.empty import EmptyOperator
+from pendulum import datetime, duration
+
 
 
 """
@@ -12,18 +13,19 @@ from pendulum import datetime, timedelta
 
 @dag(start_date = datetime(2025,1,1),
      schedule = '@daily',
-     catchup = False
-     description:"This DAG processes ecommerce data",
+     catchup = True, #only 1 DAG run for True and False. catchup look at the lasttime Dg was triggered->today
+     description ="This DAG processes ecommerce data",
      tags =["team_a","ecom"],
      default_args={"retries":1},
-     dagrun_timeout = timedelta(minutes =20),
-     max_consecutive_failed_dag_run =2
-     max_active_runs =1 #one dag can run at a time
+     dagrun_timeout = duration(minutes =20)
+    #  max_consecutive_failed_dag_runs_per_dag =2
+    #  max_active_runs =1 #one dag can run at a time
 )
 
 def ecom():
-    ta = PythonOperator(task_id = 'ta')
-    tb = PythonOperator(task_id = 'tb')
-    tc = PythonOperator(task_id = 'tc')
+    # ta = PythonOperator(task_id = 'ta')
+    # tb = PythonOperator(task_id = 'tb')
+    # tc = PythonOperator(task_id = 'tc')
+    ta = EmptyOperator(task_id = 'ta')
 
 ecom()
